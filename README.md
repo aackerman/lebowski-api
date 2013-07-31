4034125b432539fee9e4990985503d98a7125795
Aaron Ackerman <theron17@gmail.com>
2013-07-30 19:01:48 -0500

    Updated seeds, added gzip in application.rb

---
+++ README.md
@@ -0,0 +1,3 @@
+# [lebowski.me](http://lebowski.me)
+
+A small rails app for serving up information from The Big Lebowski
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(:default, Rails.env)
 
 module LebowskiApi
   class Application < Rails::Application
+    config.secret_key_base = 'beepboop'
+    config.middleware.use Rack::Deflater
     config.exceptions_app = self.routes
   end
 end
