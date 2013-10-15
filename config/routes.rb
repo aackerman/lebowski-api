e242881429b44915cac02782722db943d66bf4be
Aaron Ackerman <theron17@gmail.com>
2013-10-15 17:35:26 -0500

    Use resources in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 LebowskiApi::Application.routes.draw do
   namespace :api do
-    get 'quotes/random', to: 'quotes#random'
-    get 'quotes/:id',    to: 'quotes#show',  as: 'quote'
+    resources :quotes, only: [:show]
+    get 'random', to: 'quotes#random'
+    resources :lines, only: [:show]
     get 'lines/random',  to: 'lines#random'
-    get 'lines/:id',     to: 'lines#show',   as: 'line'
     get 'search',        to: 'search#show'
   end
 
