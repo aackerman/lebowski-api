76095ee5b2f6b156f0fc6d9f9d4a7661ff2dd9f7
Aaron Ackerman <theron17@gmail.com>
2013-10-21 22:01:15 -0500

    Setup http caching for script controller

--- app/controllers/api/script_controller.rb
+++ app/controllers/api/script_controller.rb
@@ -1,5 +1,6 @@
 class Api::ScriptController < ApplicationController
   def show
+    expires_in 1.year, public: true
     render json: Line.includes(:character, :quote).to_a, root: :script
   end
 end
