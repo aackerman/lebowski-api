22e471fc0973b74569fbb450e806213f4f2252ef
Aaron Ackerman <theron17@gmail.com>
2013-10-22 07:09:22 -0500

    Use plural searches instead of searches

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,6 +1,6 @@
 class Api::SearchController < ApplicationController
   def show
-    @search = Line.search(params[:term]).joins(:quote).includes(:character, :quote).map(&:quote)
-    render json: @search
+    @searches = Line.search(params[:term]).joins(:quote).includes(:character, :quote).map(&:quote)
+    render json: @searches, root: :searches
   end
 end
