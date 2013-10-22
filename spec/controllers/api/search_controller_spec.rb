140dec5affdbd2cfce4a57581b810b778d44eee4
Aaron Ackerman <theron17@gmail.com>
2013-10-21 22:02:16 -0500

    Fixed search_controller spec

--- spec/controllers/api/search_controller_spec.rb
+++ spec/controllers/api/search_controller_spec.rb
@@ -10,7 +10,7 @@ describe Api::SearchController do
 
       it 'gets the specified quote' do
         expect(Line).to receive(:search).with(term).and_return(helper)
-        expect(helper).to receive(:includes).with(:quote).and_return([])
+        expect(helper).to receive(:joins).with(:quote).and_return([])
         get :show, { term: term }
       end
     end
