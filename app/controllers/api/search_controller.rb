e719e610a373f04b1ec955db2b03f9db4f398800
Aaron Ackerman <theron17@gmail.com>
2013-10-25 18:19:23 -0500

    Call uniq! to get unique searches deal with perf later

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,6 +1,6 @@
 class Api::SearchController < ApplicationController
   def show
-    @searches = Line.search(params[:term]).joins(:quote).includes(:character, :quote).map(&:quote)
+    @searches = Line.search(params[:term]).joins(:quote).includes(:character, :quote).map(&:quote).uniq!
     render json: @searches, root: :searches
   end
 end
