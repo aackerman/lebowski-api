a06161fb13df765b82b1ee3d2141cde2fec9e49b
Aaron Ackerman <theron17@gmail.com>
2013-11-10 10:03:36 -0600

    Always set branch to master for deploys instead of asking

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,17 +2,17 @@ set :application, 'lebowski-api'
 set :repo_url, 'git@github.com:aackerman/lebowski-api.git'
 set :user, 'deploy'
 set :pty, true
-ask :branch, 'master'
+set :branch, 'master'
 set :keep_releases, 5
 set :deploy_to, '/var/www/lebowski-api'
 set :deploy_via, :remote_cache
 set :ssh_options, :forward_agent => true
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{log}
 role :all, %w{lebowski.me}
 role :app, %w{lebowski.me}
 role :web, %w{lebowski.me}
 role :db, %w{lebowski.me}
-set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{log}
 
 namespace :deploy do
   desc 'Restart application'
