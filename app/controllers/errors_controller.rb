14a24d2d144e4455c83d35c8167988c74a014211
Nick Colgan <nick.colgan@gmail.com>
2013-11-15 16:59:10 -0600

    Return appropriate status code on error

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,9 @@
 class ErrorsController < ApplicationController
   def not_found
-    render :json => { status: "Error", message: "Route does not exist" }
+    render :json => { status: "Error", message: "Route does not exist" }, :status => 404
   end
 
   def exception
-    render :json => { status: "Error", message: "A general error has occured and we will look into it." }
+    render :json => { status: "Error", message: "A general error has occured and we will look into it." }, :status => 500
   end
 end
