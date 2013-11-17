da20b8c40fe09947d61b664d5909e3f494b10e4f
Aaron Ackerman <theron17@gmail.com>
2013-11-17 12:21:00 -0600

    Reformat deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,18 @@
-set :application, 'lebowski-api'
-set :repo_url, 'git@github.com:aackerman/lebowski-api.git'
-set :user, 'deploy'
-set :pty, true
-set :branch, 'master'
-set :keep_releases, 5
-set :deploy_to, '/var/www/lebowski-api'
-set :deploy_via, :remote_cache
-set :ssh_options, :forward_agent => true
+set :user,         'deploy'
+set :branch,       'master'
+set :application,  'lebowski-api'
+set :deploy_to,    '/var/www/lebowski-api'
+set :repo_url,     'git@github.com:aackerman/lebowski-api.git'
+set :deploy_via,   :remote_cache
+set :ssh_options,  forward_agent: true
+set :pty,          true
+set :keep_releases 5
 set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{log}
-role :all, %w{lebowski.me}
-role :app, %w{lebowski.me}
-role :web, %w{lebowski.me}
-role :db, %w{lebowski.me}
+set :linked_dirs,  %w{log}
+role :all,         %w{lebowski.me}
+role :app,         %w{lebowski.me}
+role :web,         %w{lebowski.me}
+role :db,          %w{lebowski.me}
 
 namespace :deploy do
   desc 'Restart application'
