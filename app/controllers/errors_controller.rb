504a6078131c8cef9ce69b7773707448a4222864
Aaron Ackerman <theron17@gmail.com>
2013-11-26 21:00:06 -0600

    Update errors_controller.rb

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,21 +1,17 @@
 class ErrorsController < ApplicationController
   def not_found
     render json: {
-      errors: [
-        {
+      errors: [{
           routes: ['does not exist']
-        }
-      ]
+      }]
     }, status: 404
   end
 
   def exception
     render json: {
-      errors: [
-        {
+      errors: [{
           server: ['an uncaught exception']
-        }
-      ]
+      }]
     }, status: 500
   end
 end
