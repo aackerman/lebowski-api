d05a4b840167a190110e2e78416c8b5758d7639b
Aaron Ackerman <theron17@gmail.com>
2014-02-19 18:02:47 -0600

    Always use null store in test mode

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,6 +31,8 @@ LebowskiApi::Application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  config.cache_store = :null_store
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 end
