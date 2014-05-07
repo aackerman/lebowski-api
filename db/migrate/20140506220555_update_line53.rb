629fe917c06c99be8c8e3e8347aaf24c19c0e731
Aaron Ackerman <theron17@gmail.com>
2014-05-06 21:33:41 -0500

    Fixed text again for line

--- db/migrate/20140506220555_update_line53.rb
+++ db/migrate/20140506220555_update_line53.rb
@@ -2,8 +2,8 @@ class UpdateLine53 < ActiveRecord::Migration
   def up
     execute <<-SQL
       update lines
-        set text = 'What the fuck are you talking about?! This Chinaman is not the issue! I''m talking about drawing a line in the sand, Dude.  Across this line you do not, uh--and also, Dude, Chinaman is not the preferred uh... nomenclature, Asian-American, please.'
-      where text ilike '%This Chinaman is not the issue';
+        set text = 'What the fuck are you talking about?! This Chinaman is not the issue! I''m talking about drawing a line in the sand, Dude.  Across this line YOU DO NOT, uh--and also, Dude, Chinaman is not the preferred uh... nomenclature, Asian-American, please.'
+      where text ilike '%talking about drawing a line in the sand%';
     SQL
   end
 
@@ -11,7 +11,7 @@ class UpdateLine53 < ActiveRecord::Migration
     execute <<-SQL
       update lines
         set text = 'What the fuck are you talking about?! This Chinaman is not the issue! I''m talking about drawing a line in the sand, Dude.  Across this line you do not, uh--and also, Dude, Chinaman is not the preferred, uh. . . Asian- American.  Please.'
-      where text ilike '%This Chinaman is not the issue';
+      where text ilike '%talking about drawing a line in the sand%';
     SQL
   end
 end
