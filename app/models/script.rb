a020d47e9b0f13294b55acd07cb56a0961ff5b4f
Aaron Ackerman <theron17@gmail.com>
2014-05-30 18:32:45 -0500

    Use an instance of script to render the script

--- app/models/script.rb
+++ app/models/script.rb
@@ -1,5 +1,5 @@
 class Script
-  def self.to_json(*args)
+  def to_json(*args)
     {
       script: Rails.cache.fetch(['script', 'to-json']) do
         Line.includes(:character, :quotes).order('id ASC').map do |l|
