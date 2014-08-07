847bc297c33236a479d073b2f97cf9889b601127
Aaron Ackerman <theron17@gmail.com>
2014-08-06 20:23:38 -0500

    Fix issue #12

---
+++ db/migrate/20140807011235_update_tender_my_resignation.rb
@@ -0,0 +1,9 @@
+class UpdateTenderMyResignation < ActiveRecord::Migration
+  def change
+    execute <<-SQL
+      update lines
+        set text = 'Listen... Maude, I gotta uh... tender my resignation or whatever, ''cause um... it looks like your mother really was kidnapped after all.'
+      where text ilike '%tender my resignation%';
+    SQL
+  end
+end
