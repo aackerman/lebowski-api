9825970ccb490503075aef596ed4df7eccb4a1ae
Aaron Ackerman <theron17@gmail.com>
2014-12-22 19:29:09 -0600

    Fix calls to LineSerializer

--- app/controllers/api/lines_controller.rb
+++ app/controllers/api/lines_controller.rb
@@ -1,18 +1,18 @@
 class Api::LinesController < ApplicationController
   def show
-    render json: LineSerializer(
+    render json: LineSerializer.new(
       Line.find(params[:id])
     ).as_json
   end
 
   def search
-    render json: LineSerializer(
+    render json: LineSerializer.new(
       LineSearch.new(params[:term]).results, root: :results
     ).as_json
   end
 
   def random
-    render json: LineSerializer(
+    render json: LineSerializer.new(
       Line.random
     ).as_json
   end
