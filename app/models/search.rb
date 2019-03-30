91ce242f802cb72b984c7cdd2d98ca7b169d6cfe
Aaron Ackerman <theron17@gmail.com>
2019-03-30 09:56:44 -0500

    Fix error when searching without a character

--- app/models/search.rb
+++ app/models/search.rb
@@ -5,7 +5,7 @@ class Search
   attr_reader :term
   attr_reader :character
 
-  def initialize(term, character)
+  def initialize(term = '', character = '')
     @term      = term   || ""
     @character = character
     @results   = search || []
